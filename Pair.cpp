a4c40e851d0661133dbcc39fa87d01516f29ab8c
reddydavinci <busisatya@gmail.com>
2020-04-07 15:54:35 +0530

    Pairs

---
+++ Pair.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+
+#define fi(i,a,b) for(int i=a;i<b;i++)
+#define p pair;
+#define input(a) cin>>a
+#define print(a) cout<<a
+
+using namespace std;
+int main(){
+    pair<int,int> p;
+    return 0;
+
+}
