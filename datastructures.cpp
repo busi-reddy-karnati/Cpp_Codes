17bb8c21f51ef29e519e501a95a3d1ce3068d04a
reddydavinci <busisatya@gmail.com>
2020-04-07 12:25:22 +0530

    Data strucutres

---
+++ datastructures.cpp
@@ -0,0 +1,18 @@
+// This is kind of template that WE will be working on right now.
+
+#include<iostream>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+//This is for Sort and the functions in STL;
+using namespace std;
+
+#define fi(i,a,b) for(int i=a;i<b;i++)
+#define fd(i,a,b) for(int i=b;i>b;i--)
+
+//For Python Kind of Coding Style(The suffix p means python)
+#define len(a) a.size()
+#define printarray(a) fi(i,0,len(a))cout<<a[i]
+#define print(a) cout<<a
+#define input(a) cin>>a;
+#define sortp(a) sort(a.begin(),a.end())
