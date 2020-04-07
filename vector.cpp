7ac82cbfac31be50ce7106c08c10da7b5f395e9e
reddydavinci <busisatya@gmail.com>
2020-04-07 15:16:45 +0530

    Vectors

---
+++ vector.cpp
@@ -0,0 +1,51 @@
+// This is kind of template that WE will be working on right now.
+
+#include<iostream>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+//This is for Sort and the functions in STL;
+
+
+
+#define fi(i,a,b) for(int i=a;i<b;i++)
+#define fd(i,a,b) for(int i=b;i>b;i--)
+
+//For Python Kind of Coding Style(The suffix p means python)
+#define len(a) a.size()
+#define printarray(a) fi(i,0,len(a))cout<<a[i]
+#define print(a) cout<<a
+#define input(a) cin>>a;
+#define sortp(a) sort(a.begin(),a.end())
+#define push push_back
+
+using namespace std;
+//The main Code
+int main(){
+
+
+
+//++++++++++++++++++++++++Vector+++++++++++++++++++++++++++++++++++++
+/* Works Fine Vector
+
+/*
+Vector and tuple
+vector<datatype> variable()
+vector<int> a(10); intially the size is 10 with all zeroes
+ pair<int, int> a;
+
+
+
+
+vector<int> a;
+a={1,2,3,4};
+a.push(5);
+printarray(a);
+
+Works Fine vector
+*/
+
+
+
+//________________________________PAIR_______________________
+}
