da99414ab76bc888f66de95bb3bebdd7b30829db
reddydavinci <busisatya@gmail.com>
2020-04-10 21:22:38 +0530

    Permutations in C++

---
+++ STL_Algorithms/Permutations.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#define print(a) for(int i:a)cout<<i 
+#define intvec vector<int>
+using namespace std;
+int main(){
+    intvec a={1,2,3,4};
+    cout<<"\nNow rotating";
+    rotate(a.begin(),a.end(),a.end());
+    print(a);
+    cout <<"\nNext Permutation";
+    next_permutation(a.begin(),a.end());
+    print(a);
+    prev_permutation(a.begin(),a.end());
+    cout<<"\nPrevious Permutation";
+    print(a);
+    return 0;
+
+}
